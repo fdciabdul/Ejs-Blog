dc20f081b1daa44011acbf4ea46780ed79139f1b
abdulmuttaqin456@gmail.com <abdulmuttaqin456@gmail.com>
2021-02-15 16:33:20 +0700

    suu

---
+++ index.js
@@ -0,0 +1,23 @@
+const path=require('path');
+const fs=require('fs-extra');
+const express=require('express');
+const bodyParser=require('body-parser');
+const session=require('express-session');
+const moment=require('moment');
+const settings=require('./settings.json');
+
+var app=express();
+app.set('view engine','ejs');
+app.use('/resource',express.static('resource'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: 'asdasdasdasdasdasddasdasdasdas',
+    cookie: { maxAge: 300 * 1000 },
+    rolling: true
+}));
+require('./blog.js')(app,fs,path,moment,settings);
+require('./login.js')(app,settings);
+require('./new.js')(app,fs,path,settings);
+app.listen(process.env.port||80);
